dc96361d8b3252579a78bc705a23abc7306e9c31
Lubou Lukashevich <lukashevich.rs@gmail.com>
2022-07-04 21:10:14 +0300

    init: algoritmic tast 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function toReadable (number) {
-  
+let text = '';
+const first = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const second = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+let result = '';
+if (number === 0) {
+    result = 'zero';
+}
+else {
+     if (Math.floor(number % 100) < 20) {
+         result = first[Math.floor(number % 100)];
+        } 
+        else {
+            ((Math.floor(number % 100) !== 0) && (Math.floor(number % 100) >= 20)) ?
+            result = second[Math.floor((number % 100) / 10)] + ' ' + first[Math.floor((number % 100) % 10)] :
+            result = second[Math.floor((number % 100) / 10)];
+            }
+        if (Math.floor(number / 100) !== 0) {
+            result = first[Math.floor(number / 100)] + ' hundred ' + result; 
+        }
+     }
+
+ if (result.slice(-1) === ' ') {result = result.slice(0, -1)};
+ return result;
 }
